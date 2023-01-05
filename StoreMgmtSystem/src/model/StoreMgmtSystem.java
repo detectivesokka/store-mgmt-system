f61c3f9e573a22a5b10c7c7fe9ac9c9847e1b80f
detective-sokka <dutt.sa@northeastern.edu>
2023-01-05 13:16:49 -0500

    Created separate work area for each role

--- StoreMgmtSystem/src/model/StoreMgmtSystem.java
+++ StoreMgmtSystem/src/model/StoreMgmtSystem.java
@@ -1,11 +1,31 @@
 package model;
 
+import model.UserAccount.*;
+
 /**
  *
  * @author saidutt
  */
 public class StoreMgmtSystem {
+
+    private UserAccountDirectory userAccountDirectory;
     
+    public StoreMgmtSystem() {
+        
+        userAccountDirectory = new UserAccountDirectory();        
+        initData();
+    }
     
+    public void initData() {
+        
+        userAccountDirectory.newUserAccount("admin", "admin", 0);
+        userAccountDirectory.newUserAccount("customer", "customer", 1);
+        userAccountDirectory.newUserAccount("ocustomer", "ocustomer", 2);
+    }
+    
+    public UserAccountDirectory getUserAccountDirectory() {
+        
+        return userAccountDirectory;
+    }          
     
 }
