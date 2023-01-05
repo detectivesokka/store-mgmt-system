6ebbe2fd2d0a686038c3c1a226a0fd0859a17b48
detective-sokka <dutt.sa@northeastern.edu>
2023-01-05 13:16:23 -0500

    Modified UserAccount and UA Directory

--- StoreMgmtSystem/src/model/UserAccount/UserAccount.java
+++ StoreMgmtSystem/src/model/UserAccount/UserAccount.java
@@ -6,5 +6,75 @@ package model.UserAccount;
  */
 public class UserAccount {
     
+    private String userName;
+    private String password;
+    private int role;
+    private final int userId;
     
+    UserAccount(int pUserId) {
+        
+        this.userId = pUserId;        
+    }
+    
+    UserAccount(int pUserId, String pUserName, String pPassword, int pRole) {
+        
+        this.userId = pUserId;
+        this.userName = pUserName;
+        this.password = pPassword;
+        this.role = pRole;
+    }
+    
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }           
+
+    public String getRole() {
+        
+        String roleName;
+        
+        switch(this.role) {
+            
+            case 0 : roleName = "Admin";
+            break;
+            case 1 : roleName = "Customer";
+            break;
+            case 2 : roleName = "Online Customer";
+            break;
+            case 3 : roleName = "Online Delivery Man";
+            break;
+            case 4 : roleName = "Goods Manufacturer";
+            break;
+            case 5 : roleName = "Shop Manager";
+            break;
+            case 6 : roleName = "Shop Employee";
+            break;
+            case 7 : roleName = "Govt Tax Auditor";
+            break;
+            case 8 : roleName = "Goods Transporter";
+            break;
+            case 9 : roleName = "Goods Manufacturer";
+            break;
+            case 10 : roleName = "Goods Distributor";
+            break;
+            default: roleName = "";
+        }
+        
+        return roleName;
+    }
+    public void setRole(int pRole) {
+        
+        this.role = pRole;
+    }
 }
--- StoreMgmtSystem/src/model/UserAccount/UserAccountDirectory.java
+++ StoreMgmtSystem/src/model/UserAccount/UserAccountDirectory.java
@@ -1,9 +1,53 @@
 package model.UserAccount;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author saidutt
  */
 public class UserAccountDirectory {
     
+    private ArrayList<UserAccount> userAccountList;
+    private int userCount = 1;
+    
+    public UserAccountDirectory() {
+        userAccountList = new ArrayList();
+    }
+
+    public ArrayList<UserAccount> getUserAccountList() {
+        return userAccountList;
+    }
+    
+    public UserAccount searchUser(String name) {
+        
+        for(UserAccount e : this.userAccountList){
+            
+            if(e.getUserName().equals(name)) {
+                
+                return e;
+            }
+        }       
+        
+        return null;
+    }
+    
+    public void deleteUser (String name) { 
+        
+        UserAccount e;                
+        
+        e = searchUser(name);
+        
+        if (e != null) {
+                        
+            this.userAccountList.remove(e);                        
+        }                
+    }
+
+    public UserAccount newUserAccount(String pUserName, String pPassword, int pRole) {
+        
+        UserAccount ua = new UserAccount(userCount++, pUserName, pPassword, pRole);
+        this.userAccountList.add(ua);
+        return ua;        
+    }
 }
